6861770f43bab8ad9c6af7de163b48bb6f98a9c7
ravi.mali <ravi.mali@pixelnx.com>
2021-08-25 19:05:23 +0530

    edit

--- routes/index.js
+++ routes/index.js
@@ -98,9 +98,9 @@ router.post("/login",async(req,res)=>{
         var data = pixelschema(mybodydata);
         data.save(function(err){
           if(err){
-            res.render('addpage',{message:'user registered not succesful'})
+            res.render('users',{message:'user registered not succesful'})
           }else{
-            res.render('addpage',{message:'user registered successfully'})
+            res.render('editpage',{message:'user registered successfully'})
           }
         })
       });
@@ -130,37 +130,33 @@ router.post("/login",async(req,res)=>{
 
 // });
 // get single user for edit record
-router.get('/edit/:id',(req,res)=>{
-  console.log(req.params.id);
-
-  pixelschema.findById(req.params.id,function(err,pixelschema){
-    if(err){
-      console.log(err);
-    }else{
+router.get('/editpage/:id',async(req,res)=>{
+  await pixelschema.findById(req.params.id).then(pixelschema =>{
       console.log(pixelschema);
-      res.render('users',{pixelschema:pixelschema});
-    }
-  });
+      res.render('addpage',{'pixelschema':pixelschema});
+
+  }).catch(err =>{
+    console.log(err)
+  })
 });
 
 
 // update record using POST Method
 router.post('/edit/:id',(req,res)=>{
-  console.log("Myid is" + req.params.id);
- 
+  console.log("Myid is : " + req.params.id);
+  console.log(req.body);
+ return
   const mybodydata = {
         fullName:req.body.fullName,
-        email:req.body.email,
         password:req.body.password,
-        confirmPass:req.body.confirmPass
-  }
+      }
   console.log(mybodydata)
   
   pixelschema.findByIdAndUpdate(req.params.id,mybodydata,function(err){
     if(err){
-      res.redirect('editpage'+ req.params.id);
+      res.redirect('users'+ req.params.id);
     }else{
-      res.redirect('users')
+      res.redirect('addpage')
       console.log(users)
     }
   });
@@ -169,20 +165,20 @@ router.post('/edit/:id',(req,res)=>{
 
 // Delete id
 
-router.delete('delete/:id',(req,res,next)=>{
-  console.log(req.params.id);
-  pixel.remove({_id:req.params.id})
-  .then(result =>{
-    res.status(200).json({
-      message:"successfully deleted",
-      deletedData:result
-    })
-  }).catch(err=>{
-    res.status(500).json({
-      error:(error)
-    })
-  })
-})
+// router.delete('delete/:id',(req,res,next)=>{
+//   console.log(req.params.id);
+//   pixel.remove({_id:req.params.id})
+//   .then(result =>{
+//     res.status(200).json({
+//       message:"successfully deleted",
+//       deletedData:result
+//     })
+//   }).catch(err=>{
+//     res.status(500).json({
+//       error:(error)
+//     })
+//   })
+// })
 
 
 
